d362184f0d5629b71d8da073ea4d19461781d36d
tomech <109527747+tomech4@users.noreply.github.com>
2024-11-09 21:39:20 +0100

    complete 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,39 +6,43 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function (a, b) {
-  if (a === undefined || b === undefined) {
-    return 0;
-  } else {
-    let result = a;
-    for (i = 0; i < b; i++) {
-      result += i;
-    }
-    return result;
-  }
-};
 
-const multiply = function (...numbers) {
-  result.reduce((total, current) => {
-    return total * current;
-  }, 1);
+const sum = function (a) {
+  let x = a;
+  if (x.length === 0) {
+    return x = 0;
+  }
+  else {
+    return x.reduce((total, nextValue) => total + nextValue)
+  }
+}
+
+const multiply = function (a) {
+  return a.reduce((total, current) => total * current);
 };
 
 const power = function (a, b) {
-  for (i = 0; i < b; i++) {
-    a *= a;
+  let x = a;
+  for (i = 0; i < b - 1; i++) { // "b - 1" is due to the fact that we already have first iteration equal to paramether "a"
+    a *= x;
   }
   return a;
 };
 
-const factorial = function (a) {
-  let result = a;
-  for (i = a - 1; i > 0; i--) {
-    result * i;
-  }
-  return result;
-};
 
+const factorial = function (a) {
+  let x = a;
+  if (a === 0) {
+    return 1;
+  }
+  else {
+    for (i = x - 1; i > 0; i--) { // same as for multiplication, "x - 1" is for offseting the fact that we already have first value of iteration simply from given value
+      x *= i;
+    }
+    return x;
+  }
+};
+factorial(2);
 // Do not edit below this line
 module.exports = {
   add,
