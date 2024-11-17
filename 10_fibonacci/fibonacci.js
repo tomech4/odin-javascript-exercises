734cf8e3a0575f84417fa759206782569e75d884
tomech <109527747+tomech4@users.noreply.github.com>
2024-11-17 20:06:07 +0100

    complete fibonacci excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  // we need a function that will create sequence of fibonaci numbers
+  // write it down to an Array
+  // and by providing a number it would provide a correct number from fibonaci array, which means substracting 1 from request fibonaci position as array starts from 0
+  // also the input must be always a number
 
+  const fibonacciArray = [0, 1];
+  for (let i = 2; i < 100; i++) {
+    fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+  }
+  if (typeof number === 'number') {
+    if (number < 0) {
+        return "OOPS"
+    }
+    else {return fibonacciArray[number];}
+  }
+  if (typeof number === 'string') {
+    return fibonacciArray[Number(number)];
+  }
+  
 };
 
 // Do not edit below this line
