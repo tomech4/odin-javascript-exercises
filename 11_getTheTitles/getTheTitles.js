ab978e0ed3539137826d9c9c851bb3edabd5c5b7
tomech <109527747+tomech4@users.noreply.github.com>
2024-12-02 22:27:20 +0100

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(a) {
+// create a new array
+let result = [];
+// create a loop that
+for (i = 0; i < a.length; i++) {
+    result.push(a[i].title)
+}
+return result;
+// will take length of the array of objects
+// go through every array
+// take title of array Element
+// put it into the result array
+// return result
 };
+
+//// course solution
+// const getTheTitles = function (array) {
+//     return array.map((book) => book.title);
+//   };
+  
 
 // Do not edit below this line
 module.exports = getTheTitles;
